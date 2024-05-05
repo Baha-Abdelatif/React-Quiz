cba9da1e720d7f352f7658ecf1b756834995202f
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-05-05 18:51:41 +0200

    Fetching and display questions and possible answers

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <>
       <Header />
-      <Quiz />
+      <main>
+        <Quiz />
+      </main>
     </>
   );
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,33 @@
 import React, { useState } from "react";
+import { v4 as uuid } from "uuid";
+import QUESTIONS from "../questions.js";
 
 export default function Quiz() {
-  const [currentQuestionIdx, setCurrentQuestionIdx] = useState(0);
   const [userAnswers, setUserAnswers] = useState([]);
+  const currentQuestionIdx = userAnswers.length;
 
-  return <div>Quiz</div>;
+  const handleSelectAnswer = (selectedAnswer) => {
+    setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
+  };
+
+  return currentQuestionIdx < 7 ? (
+    <div id='question'>
+      <h2>{QUESTIONS[currentQuestionIdx].text}</h2>
+      <ul id='answers'>
+        {QUESTIONS[currentQuestionIdx].answers.map((answer) => {
+          return (
+            <li className='answer' key={uuid()}>
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  ) : (
+    <div>
+      <h2>No more questions</h2>
+    </div>
+  );
 }

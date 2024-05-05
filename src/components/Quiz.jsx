3ac40ea0bf886da826e1e6b87027c2095e8ddd7b
Baha-Abdelatif <baha.abdelatif@gmail.com>
2024-05-05 19:00:38 +0200

    Shuffle the possible answers to be sure they are not in the same order every time

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -5,25 +5,29 @@ import QUESTIONS from "../questions.js";
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const currentQuestionIdx = userAnswers.length;
+  const shuffledAnswers = QUESTIONS[currentQuestionIdx].answers;
+  shuffledAnswers.sort(() => 0.5 - Math.random());
 
   const handleSelectAnswer = (selectedAnswer) => {
     setUserAnswers((prevAnswers) => [...prevAnswers, selectedAnswer]);
   };
 
   return currentQuestionIdx < 7 ? (
-    <div id='question'>
-      <h2>{QUESTIONS[currentQuestionIdx].text}</h2>
-      <ul id='answers'>
-        {QUESTIONS[currentQuestionIdx].answers.map((answer) => {
-          return (
-            <li className='answer' key={uuid()}>
-              <button onClick={() => handleSelectAnswer(answer)}>
-                {answer}
-              </button>
-            </li>
-          );
-        })}
-      </ul>
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTIONS[currentQuestionIdx].text}</h2>
+        <ul id='answers'>
+          {shuffledAnswers.map((answer) => {
+            return (
+              <li className='answer' key={uuid()}>
+                <button onClick={() => handleSelectAnswer(answer)}>
+                  {answer}
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </div>
   ) : (
     <div>
